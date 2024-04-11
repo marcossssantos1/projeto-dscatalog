37cadd5bca1182a7bed63ca6569505b0e0b95441
Marcos Santos <marcossantosmsms604@gmail.com>
2024-04-11 17:13:36 -0300

    Seeding database

---
+++ backend/dsCatalog/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) VALUES ('Shooter');
+INSERT INTO tb_category (name) VALUES ('MOBA');
+INSERT INTO tb_category (name) VALUES ('RPG');
+INSERT INTO tb_category (name) VALUES ('Battle Royale');
